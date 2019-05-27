ab96fe99aa56d0a9de449eb11b639d4d87a0e4b0
fung991159 <yeung.kei.fung@gmail.com>
2019-05-28 07:52:00 +0800

    completed get_reports :)

--- get_reports.py
+++ get_reports.py
@@ -10,38 +10,43 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 
 def get_Reports(stockCode, numOfyear = 5):
-    #grab input stockCode and use browser to search stock information on different sites
+    #Download latest n years of annual and interim reports from HKEX in headless mode
     stockCode = str(stockCode).zfill(4)
     os.makedirs(f'C:\\Users\\Fung\\Downloads\\Financial Reports {stockCode}', exist_ok=True)
     link = 'http://www3.hkexnews.hk/listedco/listconews/advancedsearch/search_active_main_c.aspx'
     opts = ChromeOptions() #make chrome stay open after test
-    opts.add_experimental_option("detach", True) #browser stay open
-    
+    opts.add_argument("--headless")
+    # opts.add_experimental_option("detach", True) #browser stay open
     driver = webdriver.Chrome(r'C:\xampp\htdocs\Python\stock_Researcher\related files\chromedriver.exe', chrome_options=opts)
-    driver.get(link)
-    driver.find_element_by_id('ctl00_txt_stock_code').send_keys(stockCode)
-    select = Select(driver.find_element_by_id('ctl00_sel_tier_1'))
-    select.select_by_value('4') 
-    select2 = Select(driver.find_element_by_id('ctl00_sel_tier_2'))
-    select2.select_by_value('159')  #annual reports
-    driver.find_element_by_xpath('//*[@id="aspnetForm"]/table/tbody/tr[7]/td[3]/label/a[1]').click() 
-    elems = driver.find_elements_by_tag_name('a')
-    AR_regex = re.compile (r'http://www3.hkexnews.hk/listedco/listconews/SEHK/(20\d\d).*\.pdf', re.IGNORECASE)
-    count = 0
-    for elem in elems:
-        if count <= numOfyear-1:
-            pdfUrl = elem.get_attribute("href")
-            if AR_regex.search(pdfUrl) is not None:
-                # pdfList.append(pdfUrl)
-                filename = AR_regex.search(pdfUrl).group(1)
-                res = requests.get(pdfUrl)
-                print(f'downloading {stockCode} annual report for year {filename}')
-                with open(os.path.join(f'C:\\Users\\Fung\\Downloads\\Financial Reports {stockCode}',filename+'.pdf'),'wb') as pdf:
-                    pdf.write(res.content)
-                count+=1
-        else:
-            break
-
+    reportList = ['159', '160'] #annual reports and interim reports
+    for report in reportList:
+        driver.get(link)
+        driver.find_element_by_id('ctl00_txt_stock_code').send_keys(stockCode)
+        select = Select(driver.find_element_by_id('ctl00_sel_tier_1'))
+        select.select_by_value('4') 
+        select2 = Select(driver.find_element_by_id('ctl00_sel_tier_2'))
+        select2.select_by_value(report)  #annual reports
+        driver.find_element_by_xpath('//*[@id="aspnetForm"]/table/tbody/tr[7]/td[3]/label/a[1]').click() 
+        elems = driver.find_elements_by_tag_name('a') #search for all pdf links in page
+        AR_regex = re.compile (r'http://www3.hkexnews.hk/listedco/listconews/SEHK/(20\d\d).*\.pdf', re.IGNORECASE) 
+        count = 0
+        for elem in elems:
+            if count <= numOfyear-1:
+                pdfUrl = elem.get_attribute("href")
+                if AR_regex.search(pdfUrl) is not None:
+                    pdfYear = AR_regex.search(pdfUrl).group(1)
+                    res = requests.get(pdfUrl)
+                    if report == '159':
+                        localFileName = f'Annual Report {pdfYear}.pdf'
+                    else:
+                        localFileName = f'Interim Report {pdfYear}.pdf'
+                    print(f'downloading {stockCode} {localFileName}')     
+                    with open(os.path.join(f'C:\\Users\\Fung\\Downloads\\Financial Reports {stockCode}', localFileName),'wb') as pdf: #download PDF files
+                        pdf.write(res.content)
+                    count+=1
+            else:
+                break
+    
 if __name__ == "__main__":
-    get_Reports(939)
-    # print('All reports downloaded!')
+    get_Reports(87)
+    print('All reports downloaded!')
