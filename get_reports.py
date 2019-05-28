495242eb8c3b91344c66281aff35f36016117d40
fung991159 <yeung.kei.fung@gmail.com>
2019-05-28 08:01:30 +0800

    minor edit

--- get_reports.py
+++ get_reports.py
@@ -10,11 +10,11 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 
 def get_Reports(stockCode, numOfyear = 5):
-    #Download latest n years of annual and interim reports from HKEX in headless mode
+    #---Download latest n years of annual and interim reports from HKEX in headless mode---
     stockCode = str(stockCode).zfill(4)
     os.makedirs(f'C:\\Users\\Fung\\Downloads\\Financial Reports {stockCode}', exist_ok=True)
     link = 'http://www3.hkexnews.hk/listedco/listconews/advancedsearch/search_active_main_c.aspx'
-    opts = ChromeOptions() #make chrome stay open after test
+    opts = ChromeOptions()
     opts.add_argument("--headless")
     # opts.add_experimental_option("detach", True) #browser stay open
     driver = webdriver.Chrome(r'C:\xampp\htdocs\Python\stock_Researcher\related files\chromedriver.exe', chrome_options=opts)
@@ -25,16 +25,16 @@ def get_Reports(stockCode, numOfyear = 5):
         select = Select(driver.find_element_by_id('ctl00_sel_tier_1'))
         select.select_by_value('4') 
         select2 = Select(driver.find_element_by_id('ctl00_sel_tier_2'))
-        select2.select_by_value(report)  #annual reports
+        select2.select_by_value(report)  #reports type selection
         driver.find_element_by_xpath('//*[@id="aspnetForm"]/table/tbody/tr[7]/td[3]/label/a[1]').click() 
         elems = driver.find_elements_by_tag_name('a') #search for all pdf links in page
         AR_regex = re.compile (r'http://www3.hkexnews.hk/listedco/listconews/SEHK/(20\d\d).*\.pdf', re.IGNORECASE) 
         count = 0
         for elem in elems:
-            if count <= numOfyear-1:
+            if count <= numOfyear-1: #default getting 5 year/period report
                 pdfUrl = elem.get_attribute("href")
                 if AR_regex.search(pdfUrl) is not None:
-                    pdfYear = AR_regex.search(pdfUrl).group(1)
+                    pdfYear = AR_regex.search(pdfUrl).group(1) #current doc year as shown in pdfUrl
                     res = requests.get(pdfUrl)
                     if report == '159':
                         localFileName = f'Annual Report {pdfYear}.pdf'
@@ -45,8 +45,8 @@ def get_Reports(stockCode, numOfyear = 5):
                         pdf.write(res.content)
                     count+=1
             else:
-                break
-    
+                break #don't continue with other links
+    os.startfile(f'C:\\Users\\Fung\\Downloads\\Financial Reports {stockCode}') #open report folder
 if __name__ == "__main__":
-    get_Reports(87)
+    get_Reports(87) #1st arg=stockCode, 2nd opt arg=num of yr report to get
     print('All reports downloaded!')
